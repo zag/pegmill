bc8279f875dd67389d890221c6edf5c3257dae7b
Alexander Zahatski <zag@cpan.org>
2026-02-26 22:08:29 +0100

    chore: add eslint.config.js and jest.config.js tooling

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+"use strict";
+
+module.exports = {
+  "collectCoverage": true,
+  "coverageReporters": ["lcov", "text"],
+  "coveragePathIgnorePatterns": [
+    "<rootDir>/node_modules/",
+    "<rootDir>/test",
+  ],
+  "roots": [
+    "<rootDir>/test",
+  ],
+  "testMatch": [
+    "**/*.spec.js",
+    "**/*.spec.ts",
+    "**/*.test-d.ts",
+  ],
+  "transform": {
+    "^.+\\.ts$": ["ts-jest", { "tsconfig":"test/tsconfig.json" }],
+  },
+  "testTimeout": 20000,
+};
