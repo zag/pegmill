8e3355b43c1d45ccdde109bf24f9e69472b7f933
Alexander Zahatski <zag@cpan.org>
2026-03-03 20:54:58 +0100

    feat(pass): complete Phase 1 parametric rules pipeline

---
+++ spec/unit/compiler/passes/remove-non-instantiated-templates.spec.js
@@ -0,0 +1,55 @@
+/* global peg */
+
+"use strict";
+
+var pass = require("../../../../lib/compiler/passes/remove-non-instantiated-templates");
+
+describe("compiler pass |removeNonInstantiatedTemplates|", function() {
+  it("removes template rules (rules with params)", function() {
+    expect(pass).toChangeAST(
+      [
+        'List<Item> = Item+',
+        'start = List$Word',
+        'Word = [a-z]+'
+      ].join("\n"),
+      {
+        rules: [
+          { name: "start" },
+          { name: "Word" }
+        ]
+      }
+    );
+  });
+
+  it("keeps non-template rules unchanged", function() {
+    expect(pass).toChangeAST(
+      [
+        'start = Word+',
+        'Word = [a-z]+'
+      ].join("\n"),
+      {
+        rules: [
+          { name: "start" },
+          { name: "Word" }
+        ]
+      }
+    );
+  });
+
+  it("removes multiple template rules", function() {
+    expect(pass).toChangeAST(
+      [
+        'List<Item> = Item+',
+        'Pair<A, B> = A B',
+        'start = "x"',
+        'Word = [a-z]+'
+      ].join("\n"),
+      {
+        rules: [
+          { name: "start" },
+          { name: "Word" }
+        ]
+      }
+    );
+  });
+});
